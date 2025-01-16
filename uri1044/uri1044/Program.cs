e638c7358801478b5bdaf411ea8cbfd85a14acb9
Aurélio Miguel Filho <112586520+AM89815@users.noreply.github.com>
2025-01-16 17:03:21 -0300

    Add files via upload

---
+++ uri1044/uri1044/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+1044 - Mùltiplos
+
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
+indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+*/
+
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        string[] var = Console.ReadLine().Split(' ');
+        int A = int.Parse(var[0]);
+        int B = int.Parse(var[1]);
+
+        if (A % B == 0 || B % A == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+    }
+}
---
+++ uri1046/uri1046/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+1046 - Tempo de Jogo
+
+Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e
+terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
+
+Entrada
+A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
+
+Saída
+Apresente a duração do jogo conforme exemplo abaixo.
+*/
+
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int inicio, termino, duracao = 0;
+
+        string[] var = Console.ReadLine().Split(' ');
+        inicio = int.Parse(var[0]);
+        termino = int.Parse(var[1]);
+
+        if (inicio > 24 || inicio < 0 || termino > 24 || termino < 0)
+        {
+            Console.WriteLine("INVALIDO");
+        }
+        else if (inicio == termino)
+        {
+            duracao = 24;
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+        else
+        {
+            if (termino < inicio)
+            {
+                duracao = (24 - inicio) + termino;
+            }
+            else
+            {
+                duracao = termino - inicio;
+            }
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
